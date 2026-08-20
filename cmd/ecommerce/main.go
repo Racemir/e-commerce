ecdf3f8909ae6e0cbc6b41497a875fbe200b4fc8
Racemir <fehmiemirozcann@gmail.com>
2026-08-20 10:20:29 +0300

    feat(auth,db): database'e ConnectionRedis fonkssiyonu eklendi. -Go Binary ./ecommerce server,migrate,migrate-down eklendi. -POST /api/auth/register

--- cmd/ecommerce/main.go
+++ cmd/ecommerce/main.go
@@ -15,20 +15,57 @@ func main() {
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
 		// Eğer dışarıdan adres gelmezse varsayılan adresi kullan (Geliştirme ortamı için hayat kurtarır)
-		dbURL = "postgres://admin:secretpassword@localhost:5432/ecommerce"
+		dbURL = "postgres://admin:secretpassword@localhost:5433/ecommerce?sslmode=disable"
 	}
 
-	pool, connectionPoolErr := database.ConnectionPool(dbURL)
+	// Veritabanı ile uygulama arasında bir bağlantı havuzu oluşturur.
+	// Her istekte yeni bağlantı açmak yerine bu havuzdaki hazır bağlantılar kullanılır.
+	dbPool, connectionPoolErr := database.ConnectionPool(dbURL)
 	if connectionPoolErr != nil {
 		log.Fatalf("Error ConnectionPool: %v", connectionPoolErr)
 	}
-	defer pool.Close()
+	// Veritabanı bağlantılarının güvenlice kapatılması
+	defer dbPool.Close()
 
-	mux := server.SetupRoutes()
-	fmt.Println("Server 8080 portunda çalışıyor")
-
-	listenAndServeErr := http.ListenAndServe(":8080", mux)
-	if listenAndServeErr != nil {
-		fmt.Print("Sunucu Başlatılamadı:", listenAndServeErr)
+	rdb, connectionRedisError := database.ConnectionRedis()
+	if connectionRedisError != nil {
+		log.Fatalf("Error connectionRedis: %v", connectionRedisError)
 	}
+	defer rdb.Close()
+
+	// /api/health, / gibi yolların bağlı olduğu yönlendiriciyi (mux) alıyoruz.
+	switch os.Args[1] {
+	case "server":
+		mux := server.SetupRoutes(dbPool, rdb)
+		fmt.Println("Server 8080 portunda çalışıyor")
+
+		// http.ListenAndServe: Sunucuyu belirtilen portta (8080) başlatır ve gelen istekleri dinlemeye başlar.
+		// İkinci parametre olarak hazırladığımız yönlendiriciyi (mux) veriyoruz ki istekler doğru yerlere gitsin.
+		// Bu satır bloklayıcıdır (blocking). Yani program burada sürekli bekler ve çalışmaya devam eder.
+		listenAndServeErr := http.ListenAndServe(":8080", mux)
+		// 8080 portu başka bir uygulama tarafından kullanılıyorsa
+		if listenAndServeErr != nil {
+			fmt.Print("Sunucu Başlatılamadı: %v", listenAndServeErr)
+		}
+
+	case "migrate":
+
+		runMigrationsUpError := database.RunMigrationsUp(dbURL)
+		if runMigrationsUpError != nil {
+			log.Fatalf("Migration up hatası: %v", runMigrationsUpError)
+		}
+
+		fmt.Println("Migration işlemi başarıyla tamamlandı.")
+
+	case "migrate-down":
+
+		runMigrationsDownError := database.RunMigrationsDown(dbURL)
+		if runMigrationsDownError != nil {
+			log.Fatalf("Migration down hatası: %v", runMigrationsDownError)
+		}
+		fmt.Println("Sistem başarıyla geri alındı")
+	default:
+		log.Fatalf("Unknown command")
+	}
+
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -5,18 +5,39 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 func ConnectionPool(dbURL string) (*pgxpool.Pool, error) {
-	pool, pgxpoolErr := pgxpool.New(context.Background(), dbURL)
-	if pgxpoolErr != nil {
-		return nil, fmt.Errorf("Connection pool could not be created./Bağlantı havuzu oluşturulamadı: %v", pgxpoolErr)
+
+	// pgxpool.New: Verilen adres (dbURL) ile yeni bir bağlantı havuzu ayarlarını yapar.
+	// context.Background(): Go'da işlemlerin yaşam süresini (timeout, iptal vb.) yöneten bağlamdır.
+	// Background(), "özel bir kısıtlaması olmayan standart bir işlem" anlamına gelir.
+	pool, pgxpoolError := pgxpool.New(context.Background(), dbURL)
+	if pgxpoolError != nil {
+		return nil, fmt.Errorf("Connection pool could not be created./Bağlantı havuzu oluşturulamadı: %v", pgxpoolError)
 	}
 
-	pingErr := pool.Ping(context.Background())
-	if pingErr != nil {
-		return nil, fmt.Errorf("Ping, database error./Ping, veritabanı hatası: %v", pingErr)
+	// pool.Ping: Veritabanına ufak bir "Orada mısın?" sinyali gönderir ve cevap bekler.
+	pingError := pool.Ping(context.Background())
+	if pingError != nil {
+		return nil, fmt.Errorf("Ping, database error./Ping, veritabanı hatası: %v", pingError)
 	}
 
 	return pool, nil
+}
+
+func ConnectionRedis() (*redis.Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Docker-compose dosyasında belirlediğimiz port
+		Password: "",               // Şifre belirlemediğimiz için boş
+		DB:       0,                // Varsayılan veritabanı numarası
+	})
+
+	// context.Background() kullanarak basit bir ping atıyoruz
+	if pingErr := rdb.Ping(context.Background()).Err(); pingErr != nil {
+		return nil, fmt.Errorf("Ping, redis error./Ping, redis hatası: %v", pingErr)
+	}
+
+	return rdb, nil
 }
